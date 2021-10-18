0393833daafc0ca4c20587c2004f4fe3eef7db14
Ron <onedarksky@icloud.com>
2021-10-18 13:00:46 +0800

    Initial Commit

---
+++ 學習使用 AVSpeechSynthesizer 講話/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  學習使用 AVSpeechSynthesizer 講話
+//
+//  Created by 江庸冊 on 2021/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
